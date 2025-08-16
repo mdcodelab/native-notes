b5aef763a2f64b311a7cbe80aa0b5baf5b66db65
miha11-hash <d_mihaela@msn.com>
2025-08-16 13:42:49 +0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,23 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
